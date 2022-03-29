dc9e98c6cf3e9b28efea678c10576d7623b2a724
cab3sudo <paulocancio02@gmail.com>
2022-03-30 00:33:25 +0100

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,10 @@ function Home(){
     return(
         <div>
             <h2>Home</h2>
-            <Contador/>
+
             </div>
     ) 
 }
 
-function Contador() {
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-
-    return(
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-        
-    
-}
 
 export default Home
